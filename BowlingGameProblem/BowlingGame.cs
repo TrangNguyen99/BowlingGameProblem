b5ae19b59576b66b51dcf1c242385e8483b81e54
Trang Nguyen Van <nguyenvantrangbn1999@gmail.com>
2023-04-14 14:31:49 +0700

    Bowling game

---
+++ BowlingGameProblem/BowlingGame.cs
@@ -0,0 +1,56 @@
+﻿namespace BowlingGameProblem
+{
+    public class BowlingGame
+    {
+        public const int FRAME = 10;
+        public const int PIN = 10;
+
+        private readonly List<int[]> Frames = new();
+
+        public void Roll(int[] frame)
+        {
+            Frames.Add(frame);
+        }
+
+        public int Score()
+        {
+            var score = 0;
+
+            for (var i = 0; i < FRAME; i++)
+            {
+                score += Frames[i].Sum();
+
+                if (i != FRAME - 1)
+                {
+                    if (IsStrike(Frames[i]))
+                    {
+                        if (Frames[i + 1].Length == 1)
+                        {
+                            score += Frames[i + 1][0] + Frames[i + 2][0];
+                        }
+                        else
+                        {
+                            score += Frames[i + 1][0] + Frames[i + 1][1];
+                        }
+                    }
+                    else if (IsSpare(Frames[i]))
+                    {
+                        score += Frames[i + 1][0];
+                    }
+                }
+            }
+
+            return score;
+        }
+
+        private static bool IsStrike(int[] frame)
+        {
+            return frame[0] == PIN;
+        }
+
+        private static bool IsSpare(int[] frame)
+        {
+            return frame[0] != PIN && frame.Sum() == PIN;
+        }
+    }
+}
---
+++ BowlingGameProblem/Program.cs
@@ -0,0 +1,33 @@
+﻿using BowlingGameProblem;
+
+// Test 1
+var bowlingGame1 = new BowlingGame();
+
+bowlingGame1.Roll(new int[] { 4, 3 });
+bowlingGame1.Roll(new int[] { 10 });
+bowlingGame1.Roll(new int[] { 4, 5 });
+bowlingGame1.Roll(new int[] { 1, 3 });
+bowlingGame1.Roll(new int[] { 0, 4 });
+bowlingGame1.Roll(new int[] { 2, 5 });
+bowlingGame1.Roll(new int[] { 8, 0 });
+bowlingGame1.Roll(new int[] { 9, 1 });
+bowlingGame1.Roll(new int[] { 6, 2 });
+bowlingGame1.Roll(new int[] { 2, 8, 3 });
+
+Console.WriteLine(bowlingGame1.Score()); // 95
+
+// Test 2
+var bowlingGame2 = new BowlingGame();
+
+bowlingGame2.Roll(new int[] { 10 });
+bowlingGame2.Roll(new int[] { 10 });
+bowlingGame2.Roll(new int[] { 10 });
+bowlingGame2.Roll(new int[] { 10 });
+bowlingGame2.Roll(new int[] { 10 });
+bowlingGame2.Roll(new int[] { 10 });
+bowlingGame2.Roll(new int[] { 10 });
+bowlingGame2.Roll(new int[] { 10 });
+bowlingGame2.Roll(new int[] { 10 });
+bowlingGame2.Roll(new int[] { 10, 10, 10 });
+
+Console.WriteLine(bowlingGame2.Score()); // 300
